e4c12ac258b834ece6a8f7600e812228bf612e1f
leventkalkavan <levent.kalkavan@hotmail.com>
2023-07-19 12:02:37 +0300

    fluentValidation with validationFilter

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -4,28 +4,40 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { AlertifyService, MessageType } from 'src/app/services/admin/alertify.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
-
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent extends BaseComponent implements OnInit {
-constructor(spiner:NgxSpinnerService,private productService : ProductService,private alertify: AlertifyService){
-  super(spiner)
-}
+  constructor(spiner: NgxSpinnerService, private productService: ProductService, private alertify: AlertifyService) {
+    super(spiner)
+  }
   ngOnInit(): void {
   }
-  create(name: HTMLInputElement,stock:HTMLInputElement,price:HTMLInputElement){
+  create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
     this.showSpinner(SpinnerType.BallAtom);
     const create_product: Create_Product = new Create_Product();
     create_product.name = name.value;
-    create_product.price=parseInt(price.value);
-    create_product.stock=stock.value; 
+    create_product.stock = stock.value;
+    create_product.price = parseFloat(price.value);
 
-    this.productService.create(create_product,()=>{
+    if (!name.value) {
+      this.alertify.message("Lütfen ürün adını giriniz!",MessageType.Error);
+      return;
+    }
+
+    if (parseInt(stock.value) < 0) {
+      this.alertify.message("Stok bilgisini doğru giriniz.",MessageType.Error);
+      return;
+    }
+
+
+    this.productService.create(create_product, () => {
       this.hideSpinner(SpinnerType.BallAtom);
-      this.alertify.message("Ürün eklenmiştir.",MessageType.Success);
+      this.alertify.message("Ürün başarıyla eklenmiştir.",MessageType.Success);
+    }, errorMessage => {
+      this.alertify.message(errorMessage,MessageType.Error);
     });
   }
 }
