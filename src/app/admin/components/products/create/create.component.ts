1b83ae98a237a67cfcff5816691eb937d557100f
leventkalkavan <levent.kalkavan@hotmail.com>
2023-07-20 16:02:49 +0300

    file upload development

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,22 +1,34 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { AlertifyService, MessageType } from 'src/app/services/admin/alertify.service';
+import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent extends BaseComponent implements OnInit {
+
   constructor(spiner: NgxSpinnerService, private productService: ProductService, private alertify: AlertifyService) {
     super(spiner)
   }
+
   ngOnInit(): void {
   }
 
   @Output() createdProduct: EventEmitter<Create_Product> = new EventEmitter();
+  @Output() fileUploadOptions: Partial<FileUploadOptions> = {
+    action: "upload",
+    controller: "products",
+    explanation: "Resimleri sürükleyin veya seçin...",
+    isAdminPage: true,
+    accept:".png, .jpg, .jpeg, .json"
+  };
 
   create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
     this.showSpinner(SpinnerType.BallAtom);
@@ -24,6 +36,14 @@ export class CreateComponent extends BaseComponent implements OnInit {
     create_product.name = name.value;
     create_product.stock = stock.value;
     create_product.price = parseFloat(price.value);
+
+    this.productService.create(create_product, () => {
+      this.hideSpinner(SpinnerType.BallAtom);
+      this.alertify.message("Ürün başarıyla eklenmiştir.", MessageType.Success);
+      this.createdProduct.emit(create_product);
+    }, errorMessage => {
+      this.alertify.message(errorMessage,MessageType.Error);
+    });
   }
 }
     // if (!name.value) {
